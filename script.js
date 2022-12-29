4fbd4e99e50881eabc5ddf14181c505494d1e8c0
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-29 13:50:16 -0800

    Add option to add new book

--- script.js
+++ script.js
@@ -2,27 +2,35 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
 
-const buttonAdd = document.querySelector('.add');
-const buttonContainer = document.querySelector('.add-container');
-const headerTitle = document.querySelector('.add-1');
-const buttonAdd2 = document.querySelector('.add-2');
+const titleWindow = document.querySelector('.title-window');
+const form = document.querySelector('.add-book-form');
 const content = document.querySelector('.content');
+const btnSumbit = document.querySelector('.submit-form');
+
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const dateInput = document.querySelector('#date');
+const ratingInput = document.querySelector('#rating');
+const statusInput = document.querySelector('#status');
 
 let myLibrary = [];
 
-function Book(title, author, date, rating) {
+function Book(title, author, date, rating, status) {
 	this.title = title;
 	this.author = author;
 	this.date = date;
 	this.rating = rating;
+	this.status = status;
 }
 
-function addBookToLibrary(title, author, date, rating) {
-	myLibrary.push(new Book(title, author, date, rating));
+function addBookToLibrary(title, author, date, rating, status) {
+	myLibrary.push(new Book(title, author, date, rating, status));
 }
 
-addBookToLibrary('Moby Dick', 'Herman Melville', '2022/04/13', 5);
-addBookToLibrary('Lolita', 'Vladmir Nabokov', '2020/01/13', 5);
+addBookToLibrary('Moby Dick', 'Herman Melville', '2022/04/13', 5, true);
+addBookToLibrary('Lolita', 'Vladmir Nabokov', '2020/01/13', 5, false);
+
+console.log(myLibrary);
 
 function displayBook(arr) {
 	arr.forEach((element) => {
@@ -30,13 +38,31 @@ function displayBook(arr) {
 			'afterbegin',
 			`
 		<div class="card">
-					<div class="title">${element.title}</div>
-					<div class="author">${element.author}</div>
-					<div class="date">${element.date}</div>
-					<div class="rating">${element.rating}</div>
+					<div class="title-card">${element.title}</div>
+					<div class="author-card">${element.author}</div>
+					<div class="date-card">${element.date}</div>
+					<div class="rating-card">${element.rating}</div>
+					<div class="status-card">${element.status}</div>
 				</div>`
 		);
 	});
 }
 
 displayBook(myLibrary);
+
+titleWindow.addEventListener('click', () => {
+	form.classList.toggle('hidden');
+});
+
+btnSumbit.addEventListener('click', (e) => {
+	e.preventDefault();
+	form.classList.toggle('hidden');
+	addBookToLibrary(
+		titleInput.textContent,
+		authorInput.textContent,
+		dateInput.textContent,
+		ratingInput.textContent,
+		statusInput.textContent
+	);
+	displayBook(myLibrary);
+});
