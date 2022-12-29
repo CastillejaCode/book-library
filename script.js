9ae813edbfda689d96b5fd3685401526f966a00e
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-29 15:57:15 -0800

    Refactor for easier display updating

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const titleWindow = document.querySelector('.title-window');
 const form = document.querySelector('.add-book-form');
 const content = document.querySelector('.content');
 const btnSumbit = document.querySelector('.submit-form');
+const btnDelete = document.querySelector('.delete-card');
 
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
@@ -30,41 +31,43 @@ function addBookToLibrary(title, author, date, rating, status) {
 addBookToLibrary('Moby Dick', 'Herman Melville', '2022/04/13', 5, true);
 addBookToLibrary('Lolita', 'Vladmir Nabokov', '2020/01/13', 5, false);
 
-function displayAllBooks(arr, last = false) {
-	if (last) {
+function displayAllBooks(arr) {
+	arr.forEach((element) => {
 		content.insertAdjacentHTML(
 			'beforeend',
 			`
-				<div class="card">
-					<div class="title-card">${arr[arr.length - 1].title}</div>
-					<div class="author-card">${arr[arr.length - 1].author}</div>
-					<div class="date-card">${arr[arr.length - 1].date}</div>
-					<div class="rating-card">${arr[arr.length - 1].rating}</div>
-					<div class="status-card">${arr[arr.length - 1].status}</div>
-				</div>`
-		);
-	} else {
-		arr.forEach((element) => {
-			content.insertAdjacentHTML(
-				'beforeend',
-				`
 		<div class="card">
 					<div class="title-card">${element.title}</div>
 					<div class="author-card">${element.author}</div>
 					<div class="date-card">${element.date}</div>
 					<div class="rating-card">${element.rating}</div>
 					<div class="status-card">${element.status}</div>
+					<button class="delete-card">X</button>
+
 				</div>`
-			);
-		});
-	}
+		);
+	});
+}
+
+function resetBooks() {
+	document.querySelectorAll('.card').forEach((element) => {
+		element.remove();
+	});
+}
+
+function updateDisplay(arr) {
+	resetBooks();
+	displayAllBooks(arr);
 }
 
 displayAllBooks(myLibrary);
+// resetBooks();
 
 titleWindow.addEventListener('click', () => {
 	form.classList.toggle('hidden');
 });
+
+// FIXME: Form is hidden due to opacity but still exists and can be moused over
 
 btnSumbit.addEventListener('click', (e) => {
 	e.preventDefault();
@@ -76,5 +79,17 @@ btnSumbit.addEventListener('click', (e) => {
 		ratingInput.value,
 		statusInput.value
 	);
-	displayAllBooks(myLibrary, true);
+	updateDisplay(myLibrary);
+});
+
+console.log(myLibrary);
+btnDelete.addEventListener('click', () => {
+	// Find index of card
+	let indexCard = btnDelete.closest('.card').dataset.index;
+
+	// Update display
+
+	// Delete element from array
+	myLibrary.splice(indexCard, 1);
+	console.log(myLibrary);
 });
